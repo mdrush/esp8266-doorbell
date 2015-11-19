08c46e2a5e77014e046ce6c2b8d9d2a30f74cd57
mdrush <mrusheen@gmail.com>
2015-11-18 19:06:21 -0800

    simplified circuit and code

--- WiFiClient/WiFiClient.ino
+++ WiFiClient/WiFiClient.ino
@@ -8,12 +8,6 @@
  */
 
 #include <ESP8266WiFi.h>
-#ifdef ESP8266
-extern "C" {
-#include "user_interface.h"
-#include "gpio.h"
-}
-#endif
 
 const char* ssid     = "your-ssid";
 const char* password = "your-password";
@@ -22,48 +16,16 @@ const char* host = "data.sparkfun.com";
 const char* streamId   = "....................";
 const char* privateKey = "....................";
 
-IPAddress ip(192, 168, 1, 100);  
+const int retryLimit = 5;
+
+// Using a static IP should save time
+IPAddress ip(192, 168, 1, 100);
 IPAddress gateway(192, 168, 1, 1);  
 IPAddress subnet(255, 255, 255, 0);  
 
-int buttonPin = 12;
-
-void setup() {
-  Serial.begin(115200);
-  delay(10);
-
-  // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  WiFi.config(ip, gateway, subnet); 
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  pinMode(buttonPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(buttonPin), sendNotification, LOW);
-
-  //gpio_pin_wakeup_enable(buttonPin, GPIO_PIN_INTR_LOLEVEL);
-  
-  //wifi_set_sleep_type(LIGHT_SLEEP_T);
-
-}
-
-void sendNotification(){
-  detachInterrupt(digitalPinToInterrupt(buttonPin));
-  
+/* Returns true if the request was sent and a reply was 
+  received properly, else returns false */
+bool sendNotification() {
   Serial.print("connecting to ");
   Serial.println(host);
   
@@ -72,7 +34,7 @@ void sendNotification(){
   const int httpPort = 80;
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
-    return;
+    return false;
   }
   
   // We now create a URI for the request
@@ -90,24 +52,70 @@ void sendNotification(){
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" + 
                "Connection: close\r\n\r\n");
-//  delay(10);
-//  
-//  // Read all the lines of the reply from server and print them to Serial
-//  while(client.available()){
-//    String line = client.readStringUntil('\r');
-//    Serial.print(line);
-//  }
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
   
   Serial.println();
-  Serial.println("closing connection");
+  Serial.println("closing connection\n");
+
+  return true;
 }
+
+
+void setup() {
+  /* Our button is on CH_PD, this will keep CH_PD high
+    during the duration of sending the data */
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+  
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid);
+  WiFi.config(ip, gateway, subnet);  
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  int i = 0;
+  bool worked = sendNotification();
+  i = 0;
+  while (!worked && i < retryLimit){
+     Serial.println("Try #" + (i+1));  
+     worked = sendNotification();
+     i++;
+  }
+ 
+  if (worked) {
+  Serial.println("Going down.");
+  digitalWrite(2, LOW);
+  }
+
+}
+
 
 //int value = 0;
 
 void loop() {
+  Serial.println("There was a problem...");
   delay(5000);
-  //++value;
-
-
 }
 
